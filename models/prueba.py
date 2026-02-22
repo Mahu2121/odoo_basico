879658f8c6b129142e3fa08faa817468fcd9db1b
Mahu2121 <hugogbmahu@gmail.com>
2026-02-22 23:31:26 +0100

    .

--- models/prueba.py
+++ models/prueba.py
@@ -5,10 +5,13 @@ from odoo import models, fields, api
 class prueba(models.Model):
 
 
-
-
     _name = 'odoo_basico.prueba'
     _description = 'modelo para probar o funcionamento de Odoo'
 
     name = fields.Char(required=True, size=20, string="Identificador de Prueba")
     description = fields.Text(string="A descripción da Prueba")
+
+    def creaRexistroInformacionPrueba(self):
+        creado_id = self.env['odoo_basico.informacion'].create({'name': 'Creado dende pedido'})
+        creado_id.descripcion = "Creado dende o modelo pedido"
+        creado_id.autorizado = False
